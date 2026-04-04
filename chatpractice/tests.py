0bd9513d352dc7324f811b6b5f096679db576b6b
calebeynon <ceynon@crimson.ua.edu>
2026-04-04 14:22:45 -0500

    Squashed 'analysis/paper/' changes from fab8e9d..f278b77
    f278b77 Update on Overleaf.
    01f003f Update on Overleaf.
    190f80e Update on Overleaf.
    551ca69 Update on Overleaf.
    ca6dc15 Update on Overleaf.
    43f5925 Update on Overleaf.
    
    git-subtree-dir: analysis/paper
    git-subtree-split: f278b77e087ce50930d5bc562b2e69f5297f54a8

--- chatpractice/tests.py
+++
@@ -1,9 +0,0 @@
-from otree.api import Currency as c, currency_range, expect, Bot
-from . import *
-
-
-class PlayerBot(Bot):
-    def play_round(self):
-        yield chatExplanation
-        yield Chat
-        yield RegroupingMessage
